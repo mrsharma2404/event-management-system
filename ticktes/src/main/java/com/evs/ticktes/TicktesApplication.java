241e33bd0324b47b14bf4fcfd3cd13cfa0fcd770
rohit <mrsharma2404@gmail.com>
2025-06-27 02:21:43 +0530

    initial setup with spring initializer and basic dependencies

---
+++ ticktes/src/main/java/com/evs/ticktes/TicktesApplication.java
@@ -0,0 +1,13 @@
+package com.evs.ticktes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicktesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicktesApplication.class, args);
+	}
+
+}
---
+++ ticktes/src/test/java/com/evs/ticktes/TicktesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evs.ticktes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicktesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
