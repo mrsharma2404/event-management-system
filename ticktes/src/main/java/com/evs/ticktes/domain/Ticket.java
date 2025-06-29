87114354fa6d05757e16e9cef211a2d1d22f72dd
rohit <mrsharma2404@gmail.com>
2025-06-29 19:53:21 +0530

    add Ticket and TicketType entities with relationships to Event and User

---
+++ ticktes/src/main/java/com/evs/ticktes/domain/Ticket.java
@@ -0,0 +1,50 @@
+package com.evs.ticktes.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tickets")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Ticket {
+
+    @Id
+    @Column(name = "id", nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TicketStatusEnum status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ticket_type_id")
+    private TicketType ticketType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // TODO: Validation
+
+    // TODO: QrCode
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false,  updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false, updatable = true)
+    private  LocalDateTime updatedAt;
+
+}
