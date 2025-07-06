cf49f0a5cf54b368a96fd3da280d61073e5ad3d0
rohit <mrsharma2404@gmail.com>
2025-07-06 17:44:35 +0530

    created TicketTypeRequest and linked with EventRequest

--- ticktes/src/main/java/com/evs/ticktes/domain/CreateEventRequest.java
+++ ticktes/src/main/java/com/evs/ticktes/domain/CreateEventRequest.java
@@ -1,4 +1,35 @@
 package com.evs.ticktes.domain;
 
+import com.evs.ticktes.domain.entities.EventStatusEnum;
+import com.evs.ticktes.domain.entities.TicketType;
+import com.evs.ticktes.domain.entities.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateEventRequest {
+
+    private String name;
+    private String description;
+
+    private LocalDateTime start;
+    private LocalDateTime end;
+
+    private String venue;
+
+    private LocalDateTime salesStart;
+    private LocalDateTime salesEnd;
+
+    private EventStatusEnum status;
+
+    private List<CreateTicketTypeRequest> ticketTypes =  new ArrayList<>();
+
+
 }
